fb546b45b6c93bc34686633691901f1a0d888c33
Lelo88 <villalbaleandrogabriel@gmail.com>
2023-01-06 15:41:43 -0300

    resolucion parcial testing ejercicio 02 practica 03

--- Practica/Practica03/Ejercicio02/main_test.go
+++ Practica/Practica03/Ejercicio02/main_test.go
@@ -6,26 +6,26 @@ import (
 )
 
 func TestCalculaPromedio(t *testing.T){
-	var notas = []int{5,7,8,9,12} 
-	var notas2 = []int{6,7,4,5,-1}
+	notas := []int{4,5,6,3,-1}
+	//ok1, notas2 := ingreso()
 	//ninguno de los dos resultados deberia dar ok ya que tengo un numero mayor a 10 y uno menor a 0
-	
+
 	sumNotas1 := 0
 	for _,value := range notas{
 		sumNotas1+=value
 	}
 
-	sumNotas2 := 0
+	/*sumNotas2 := 0
 	for _,value := range notas2{
 		sumNotas2+=value
-	}
+	}*/
 
 	resultadoEsperado1 := float64(sumNotas1)/float64(len(notas))
-	resultadoEsperado2 := float64(sumNotas2)/float64(len(notas2))
+	//resultadoEsperado2 := float64(sumNotas2)/float64(len(notas2))
 
 	resultado1 := calculaPromedio(notas...)
-	resultado2 := calculaPromedio(notas2...)
+	//resultado2 := calculaPromedio(notas2...)
 
 	assert.Equal(t, resultadoEsperado1, resultado1)
-	assert.Equal(t, resultadoEsperado2, resultado2)
+	//assert.Equal(t, resultadoEsperado2, resultado2, ok1)
 }
