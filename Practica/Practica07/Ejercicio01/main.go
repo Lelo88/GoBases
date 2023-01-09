cb76903bbae8c1796017b5448b7e2f2a696e6794
Lelo88 <villalbaleandrogabriel@gmail.com>
2023-01-09 12:35:12 -0300

    Ejercicio01 control de errores

---
+++ Practica/Practica07/Ejercicio01/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	defer func() {
+		if err:= recover(); err!=nil{
+			fmt.Println("Tipo de error: ", err)
+		}
+		fmt.Println("Finalizando ejecución...")
+	}()
+
+	_,err:= os.ReadFile("customers.txt")
+	if err != nil {
+		panic("No se encuentra el archivo")
+	}
+}
+
