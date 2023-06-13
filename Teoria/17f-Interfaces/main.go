95aabdaa01dcda822fdd59f2d2a16dd46f7fdb24
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2023-06-12 22:02:08 -0300

    nueva interface

---
+++ Teoria/17f-Interfaces/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//interface
+type OperacionAritmetica interface{
+	resultado() int64
+}
+
+//struct operacion suma
+type suma struct{
+	numero1 int64
+	numero2 int64
+}
+
+func (s suma) resultado() int64{
+	return s.numero1 + s.numero2
+}
+
+// struct operacion resta
+type resta struct{
+	numero1 int64
+	numero2 int64
+}
+
+func (s resta) resultado() int64{
+	return s.numero1 - s.numero2
+}
+
+func Operacion (o OperacionAritmetica) {
+	fmt.Println("El resultado de la operación es: ", o.resultado())
+}
+
+func main() {
+	Operacion(suma{numero1: 10, numero2: 20})
+	Operacion(resta{numero1: 10, numero2: 20})
+}	
