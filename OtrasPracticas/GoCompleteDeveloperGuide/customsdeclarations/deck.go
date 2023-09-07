690449e59442ecdb89a373bb3560bd9448d096fd
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2023-09-07 16:55:38 -0300

    add newcard construtctor (#1)

--- OtrasPracticas/GoCompleteDeveloperGuide/customsdeclarations/deck.go
+++ OtrasPracticas/GoCompleteDeveloperGuide/customsdeclarations/deck.go
@@ -5,8 +5,24 @@ import "fmt"
 // Create a new type of deck which is a slice of strings.
 type deck []string
 
+// This works like a constructor
+func newDeck() deck{
+	cards := deck{}
+	
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+
+	for _, suit := range cardSuits {
+		for i := 1; i <= 13; i++ {
+			card := fmt.Sprintf("%v of %v", i, suit)
+			cards = append(cards, card)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
