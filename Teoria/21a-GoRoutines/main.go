6c3e3dd59897c39383c974b9a95cb5c6e51e9545
Leandro Villalba <leandro.villalba@mercadolibre.com>
2024-12-04 09:54:37 -0300

    conceptos channel/concurrencia en golang (#8)

--- Teoria/21a-GoRoutines/main.go
+++ Teoria/21a-GoRoutines/main.go
@@ -28,3 +28,15 @@ func main() {
 	//al quitarle las dos acciones de arriba, el programa va a finalizar antes de que las go routines se ejecuten. 
 	//para eso vamos a agregarle channels
 }
+
+// La concurrencia en golang se logra con la palabra reservada go. Se van ejecutando procesos en simultaneo.
+// En este caso, se ejecutan 10 procesos en simultaneo, pero el programa finaliza antes de que se ejecuten todos los procesos.
+// Para eso se utilizan los channels.
+// Un channel es un tipo de dato que permite la comunicación entre goroutines.
+// Se pueden enviar y recibir datos a través de un channel.
+// Se crean con la función make() y se utilizan con la flecha <-
+// En este caso, se va a crear un channel de tipo int, y se va a enviar el valor i a través del channel.
+// Luego, se va a recibir el valor del channel y se va a imprimir.
+// Se va a utilizar un channel para que el programa espere a que todas las goroutines terminen de ejec
+//utar antes de finalizar el programa.
+
