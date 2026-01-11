d8268d35c7431e62328139cf45e63ce1c7488312
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2026-01-11 01:29:24 -0300

    explicacion slices

--- Teoria/12-Slices/12-Slices.go
+++ Teoria/12-Slices/12-Slices.go
@@ -19,14 +19,21 @@ func main(){
 	}
 
 	for _, value := range sliceInt{
-		fmt.Println(value)
+		fmt.Println(value) // value es el valor del slice
 	}
 
-	fmt.Println(sliceInt[1:4]) 
+	fmt.Println(sliceInt[1:4]) // sliceInt[1:4] es un slice que contiene los elementos desde el índice 1 hasta el índice 3
 
 	fmt.Println("Nuevo slice: ", sliceInt2)
 	fmt.Println("Tamaño de sliceInt2: ", len(sliceInt2), "capacidad: ", cap(sliceInt2))
-	sliceInt2 = append(sliceInt2, 6,7,8)
-	fmt.Println("Nuevo valor del nuevo slice: ", sliceInt2)
+	sliceInt2 = append(sliceInt2, 6,7,8) // append agrega elementos al slice
+	fmt.Println("Nuevo valor del nuevo slice: ", sliceInt2) // sliceInt2 ahora tiene 8 elementos
 	fmt.Println("Tamaño de sliceInt2: ", len(sliceInt2), "capacidad: ", cap(sliceInt2)) //la capacidad aumenta por realocacion
+	
+	slice2 := append(sliceInt2, 9, 10, 11)
+	fmt.Println("Nuevo slice: ", slice2)
+	fmt.Println("Tamaño de slice2: ", len(slice2), "capacidad: ", cap(slice2))
+	// La capacidad del slice2 es 10, que es el doble de la capacidad del sliceInt2
+	// Esto se debe a que Go duplica la capacidad cuando se supera el límite
+	// Al agregar más elementos, Go duplica la capacidad para evitar realocaciones frecuentes. 
 }
